615754cb9690e9bf9e25e5a23ffcf37d70b6cb49
Ata <ataserb@gmail.com>
2023-09-17 21:40:58 +0300

    add Navbar, Providers, SignInButton, ThemeToggle, UserAccountNav, UserAvatar components with helper shadcn-ui components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,31 @@
+import SignInButton from "@/components/SignInButton";
 import { Button } from "@/components/ui/button";
-import { prisma } from "@/lib/db";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+} from "@/components/ui/card";
 
 export default function Home() {
-	return <Button> Hello World </Button>;
+	return (
+		<div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+			<Card className="w-[300px]">
+				<CardHeader>
+					Welcome to QuizMate
+					<CardDescription>
+						<p>
+							QuizMate is a web app that allows you to quiz
+							yourself on any subject you want! You can create
+							your own quizzes or use quizzes created by other
+							users.
+						</p>
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<SignInButton text="Sign In with Google" />
+				</CardContent>
+			</Card>
+		</div>
+	);
 }
